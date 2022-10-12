350b854e934ae1498599af4135391672269cf477
Mohammed <sajidcurious9@gmail.com>
2022-10-13 03:49:20 +0530

    confusion day

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Textform(props) {
+  const convertToUppercase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter the text here");
   return (
     <form>
-      <div class="mb-3">
+      <div className="mb-3">
         <h1>{props.heading}</h1>
-        <textarea type="email" class="form-control" id="mybox" rows="8" />
-        <button className="primary-btn my-3 mx-8">Convert to Uppercase</button>
+        <textarea
+          type="email"
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="mybox"
+          rows="8"
+        />
+        <button className="primary-btn my-3 mx-8" onClick={convertToUppercase}>
+          Convert to Uppercase
+        </button>
         <button className="secondary-btn my-3 mx-5">
           Convert to Lowercase
         </button>
